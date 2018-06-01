343f87dc2f030033ef868ddbf38aeea001639c6b
Prodip Sarker <prodip_sarker@diu.edu.bd>
2018-06-01 16:37:52 +0600

    C#

---
+++ Threading/Concepts/Concepts/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Concepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread= new Thread(PrintHelloWorld);
+            thread.Start();
+            thread.Join();
+            Console.WriteLine("Print Hello World");
+            Console.ReadKey();
+        }
+
+        private static void PrintHelloWorld()
+        {
+            Console.WriteLine("Hello World");
+            Thread.Sleep(5000);
+        }
+    }
+}
---
+++ Threading/ContextSwitching/ContextSwitching/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ContextSwitching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread= new Thread(StartWorkerThread);
+            thread.Start();
+
+
+            //Main Thread..........
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(" A" + i + " ");
+            }
+        }
+
+        private static void StartWorkerThread()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(" Z"+i+" ");
+            }
+        }
+    }
+}
